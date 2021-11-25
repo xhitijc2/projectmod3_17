5224f87414a0cc6aacaebfad6fce3c9a5c263cb7
xhitijc2 <68293611+xhitijc2@users.noreply.github.com>
2021-11-26 04:41:06 +0530

    threradc

--- projmod#3_OS-20211125T213946Z-001/projmod#3_OS/project-module-3-group17_cs301/src/userprog/ccc.c
+++ projmod#3_OS-20211125T213946Z-001/projmod#3_OS/project-module-3-group17_cs301/src/userprog/ccc.c
@@ -1,96 +1,22 @@
-static bool
-setup_stack(void **esp, char *argumentfilenames)
+struct thread *
+lookup_tid(tid_t threadID)
 {
-    if (esp == NULL || argumentfilenames == NULL)
-        return false;
+    struct list_elem *elemnt;
+    enum intr_level olderLevel;
 
-    uint8_t *kernelPage;
-    bool result = false;
-    uint8_t *startt = ((uint8_t *)PHYS_BASE) - PGSIZE;
-    kernelPage = vm_frame_alloc(PAL_USER | PAL_ZERO, startt);
-    if (kernelPage != NULL)
+    olderLevel = intr_disable();
+    ASSERT(intr_get_level() == INTR_OFF);
+    for (elemnt = list_begin(&all_list); elemnt != list_end(&all_list);
+         elemnt = list_next(elemnt))
     {
-        result = install_page(startt, kernelPage, true);
-        if (result)
-            *esp = PHYS_BASE;
-        else
+        struct thread *currthread = list_entry(elemnt, struct thread, allelem);
+        if (currthread->threadID == threadID)
         {
-            vm_frame_free(kernelPage);
-            return false;
+            intr_set_level(olderLevel);
+            return currthread;
         }
     }
-    else
-        return false;
+    intr_set_level(olderLevel);
 
-    char *savepoitner;
-    char *tkn;
-    char *argumentoffilenamescopy;
-    int argumentcount = 0, j = 0;
-
-    argumentoffilenamescopy = malloc((strlen(argumentfilenames) + 1) * sizeof(char));
-
-    if (argumentoffilenamescopy == NULL)
-        return false;
-
-    strlcpy(argumentoffilenamescopy, argumentfilenames, strlen(argumentfilenames) + 1);
-
-    for ((tkn = strtok_r(argumentoffilenamescopy, " ", &savepoitner)); tkn != NULL && argumentcount < ARG_MAX;
-         tkn = strtok_r(NULL, " ", &savepoitner))
-    {
-        argumentcount++;
-    }
-
-    char **argvector = malloc((argumentcount + 1) * sizeof(char *));
-
-    if (argvector == NULL)
-        return false;
-    /* Load arguments*/
-    for (tkn = strtok_r(argumentfilenames, " ", &savepoitner); tkn != NULL;
-         tkn = strtok_r(NULL, " ", &savepoitner))
-    {
-        *esp -= strlen(tkn) + 1;
-        if (*esp > PHYS_BASE - PGSIZE)
-            memcpy(*esp, tkn, strlen(tkn) + 1);
-
-        argvector[j] = (char *)*esp;
-        j++;
-    }
-    argvector[j] = NULL;
-
-    /* Word alignment */
-    while ((int)*esp % 4 != 0)
-    {
-        *esp -= sizeof(char);
-
-        uint8_t zero = 0;
-        if (*esp > PHYS_BASE - PGSIZE)
-            memcpy(*esp, &zero, sizeof(char));
-    }
-
-    /* argvector pointers (must load in reversed order) */
-    for (int j = argumentcount; j >= 0; j--)
-    {
-        *esp -= sizeof(char *);
-        if (*esp > PHYS_BASE - PGSIZE)
-            memcpy(*esp, &argvector[j], sizeof(char *));
-    }
-
-    /* Load pointer to argvector */
-    void *espb4 = *esp;
-    *esp -= sizeof(char **);
-    if (*esp > PHYS_BASE - PGSIZE)
-        memcpy(*esp, &espb4, sizeof(char **));
-
-    *esp -= sizeof(int);
-    if (*esp > PHYS_BASE - PGSIZE)
-        memcpy(*esp, &argumentcount, sizeof(int));
-    *esp -= sizeof(uint32_t);
-    uint32_t ret = 0;
-    if (*esp > PHYS_BASE - PGSIZE)
-        memcpy(*esp, &ret, sizeof(uint32_t));
-
-    free(argvector);
-    free(argumentoffilenamescopy);
-
-    return result;
+    return 0;
 }
